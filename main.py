85697bc0883076ec930cb7b50c2961a5f14ae7af
Fredrik Haglund <fr3h4g@gmail.com>
2023-10-12 16:41:19 +0200

    soft reset on error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import github_update
 import urequests
 import time
 import settings
+import machine
 
 
 def update_infuxdb(data):
@@ -11,22 +12,46 @@ def update_infuxdb(data):
     token = f"Bearer {settings.influxdb_token}"
     headers = {"Authorization": token}
 
-    response = urequests.post(settings.influxdb_url, data=data, headers=headers)
+    try:
+        response = urequests.post(settings.influxdb_url, data=data, headers=headers)
+    except OSError as exc:
+        print(f"Error: {exc}")
+        return False
+    response.close()
+
     if response.status_code == 204:
         print("Done")
+        return True
     else:
         print(f"Error: {response.status_code} - {response.text}")
-    response.close()
+        return False
+
+
+def get_rssi_level():
+    return wlan.status("rssi")
 
 
 if __name__ == "__main__":
     wlan = wifi.connect()
     github_update.update_firmware()
 
+    x = 0
+    error_count = 0
     while True:
-        rssi = wlan.status("rssi")
-        data = f"Test RSSI={rssi}"
+        if x > 10000:
+            rssi = get_rssi_level()
+            data = f"Test RSSI={rssi}"
 
-        update_infuxdb(data)
+            sent_ok = update_infuxdb(data)
+            if not sent_ok:
+                error_count += 1
+                print(f"Error count: {error_count}")
+            else:
+                error_count = 0
+            x = 0
 
-        time.sleep(10)
+            if error_count >= 10:
+                machine.soft_reset()
+
+        time.sleep_ms(1)
+        x += 1
